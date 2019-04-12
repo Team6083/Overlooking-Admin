ae42637a7a181d0a7a1f2ead2da264e029d14c25
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2019-04-13 03:25:53 +0800

    add web pages

---
+++ src/main/resources/web/assets/js/main.js
@@ -0,0 +1,36 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDqGsMpbk3UFSDgJdBTp6hx1jGtZMFAvjg",
+    authDomain: "overlooking-admin.firebaseapp.com",
+    databaseURL: "https://overlooking-admin.firebaseio.com",
+    projectId: "overlooking-admin",
+    storageBucket: "overlooking-admin.appspot.com",
+    messagingSenderId: "660461854849"
+};
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        console.log("User login: ", user.email);
+        console.log(user.providerData);
+    } else {
+        // No user is signed in.
+        console.log("User logout");
+    }
+});
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function signOut() {
+    firebase.auth().signOut().then(function () {
+        // Sign-out successful.
+    }).catch(function (error) {
+        console.error(error);
+    });
+}
